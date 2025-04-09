e28b3cd44b7dcbbee74e2d3846e4bab2e4b53e21
FILTER8 <filter8@gmx.net>
2025-04-09 23:55:32 +0200

    frame verification

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,22 +1,24 @@
+import { NextResponse } from "next/server";
+
 export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL;
 
-  return Response.json({
+  return NextResponse.json({
     accountAssociation: {
       header: process.env.FARCASTER_HEADER,
       payload: process.env.FARCASTER_PAYLOAD,
       signature: process.env.FARCASTER_SIGNATURE,
     },
     frame: {
-      version: process.env.NEXT_PUBLIC_VERSION,
-      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-      homeUrl: URL,
-      iconUrl: process.env.NEXT_PUBLIC_ICON_URL,
-      imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
-      buttonTitle: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
-      splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
-      splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
-      webhookUrl: `${URL}/api/webhook`,
+      version: process.env.NEXT_PUBLIC_VERSION || "next",
+      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || "mintbay",
+      homeUrl: URL || "https://mintbay-frame.vercel.app/",
+      iconUrl: process.env.NEXT_PUBLIC_ICON_URL || "https://filter8.xyz/wp-content/uploads/2025/04/mintbay_icon.png",
+      imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL || "https://filter8.wordpress.com/wp-content/uploads/2025/04/mintbay-minikit-frame-1.png",
+      buttonTitle: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || "mintbay"}`,
+      splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL || "https://filter8.xyz/wp-content/uploads/2025/04/mintbay_icon.png",
+      splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR || "FFFFFF"}`,
+      webhookUrl: `${URL || "https://mintbay-frame.vercel.app"}/api/webhook`,
     },
   });
 }
